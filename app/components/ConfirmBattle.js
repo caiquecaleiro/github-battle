6f50547c43a4b65d4978bae315675a48c11f1a76
Caique Caleiro <caiquecaleiro@gmail.com>
2016-09-08 19:46:51 -0300

    Add onInitiateBattle

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,9 +1,42 @@
 var React = require('react');
+var PropTypes = React.PropTypes;
+var styles = require('../styles');
+var Link = require('react-router').Link;
 
 function ConfirmBattle(props) {
   return props.isLoading === true
     ? <p> LOADING! </p>
-    : <p>CONFIRM BATTLE</p>
+    : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-6">
+            <p className="lead">Player 1</p>
+            PLAYER 1 INFO
+          </div>
+          <div className="col-sm-6">
+            <p className="lead">Player 2</p>
+            PLAYER 2 INFO
+          </div>
+        </div>
+        <div className="col-sm-8 col-sm-offset-2">
+          <div className="col-sm-12" style={styles.space}>
+            <button type="button" className="btn btn-lg btn-success" onClick={props.onInitiateBattle}>
+              Initiate battle!
+            </button>
+          </div>
+          <div className="col-sm-12" style={styles.space}>
+            <Link to="/playerOne">
+              <button type="button" className="btn btn-lg btn-danger">Reselect players</button>
+            </Link>
+          </div>
+        </div>
+      </div>
 }
+
+ConfirmBattle.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  playersInfo: PropTypes.array.isRequired,
+  onInitiateBattle: PropTypes.func.isRequired
+};
 
 module.exports = ConfirmBattle;
