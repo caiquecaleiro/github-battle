08c6ee60a8b76423a9b62eca79b1d805ca6fb66e
Caique Caleiro <caiquecaleiro@gmail.com>
2016-10-22 20:23:45 -0200

    Add results component

--- app/utils/gitHubHelpers.js
+++ app/utils/gitHubHelpers.js
@@ -8,6 +8,34 @@ function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + parameter);
 }
 
+function getRepos(username) {
+  return axios.get('https://api.github.com/users/' + username + '/repos' + parameter + '&per_page=100');
+}
+
+function getTotalStars(repos) {
+  return repos.data.reduce(function(previous, current) {
+    return prev + current.stargazers_count;
+  }, 0);
+}
+
+function getPlayersData(player) {
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(function(totalStars) {
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+  });
+}
+
+function calculateScores(players) {
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
+}
+
 var helpers = {
   getPlayersInfo: function(players) {
     return axios.all(players.map(function(username) {
@@ -21,6 +49,16 @@ var helpers = {
     .catch(function(error) {
       console.warn('Error in getPlayersInfo', error);
     });
+  },
+  battle: function(players) {
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function(error) { 
+        console.warn('Error in getPlayersInfo: ' + error)
+      });
   }
 };
 
